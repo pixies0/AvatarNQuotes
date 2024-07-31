7c14f4695264e77d6f71dc3b7b53f5634692fa89
João Pedro Silva Cunha <92887485+pixies0@users.noreply.github.com>
2024-07-31 09:38:11 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const avatarAPI = 'https://api.dicebear.com/9.x/'
 export const animeAPI = axios.create({
-  baseURL: 'https://quote-garden.onrender.com/api/v3/'
+  baseURL: 'https://stoic.tekloon.net/stoic-quote/'
 })
