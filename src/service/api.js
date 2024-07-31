03e9ea9e54be8f505a0fb87dc13f67953c33bd53
João Pedro Silva Cunha <92887485+pixies0@users.noreply.github.com>
2024-07-31 09:04:35 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const avatarAPI = 'https://avatars.dicebear.com/api/'
+export const avatarAPI = 'https://api.dicebear.com/9.x/'
 export const animeAPI = axios.create({
   baseURL: 'https://animechan.vercel.app/api/'
 })
